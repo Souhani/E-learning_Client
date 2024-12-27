3387d9011dbe4f4410e8b7f56cda75226470c983
Souhani <souhanikarim1999@gmail.com>
2024-12-27 18:57:32 +0100

    deoplyment error

--- app/utils/CalculateQuizScore.ts
+++ app/utils/CalculateQuizScore.ts
@@ -19,12 +19,12 @@ import { IQuizResults } from "../components/Course/CourseQuizzes";
         // Get the correct options for the current question
         const correctOptionIds = question.options
           .filter((option) => option.is_correct)
-          .map((option) => option._id);
+          .map((option) => option._id) ?? [];
   
         // Check if the user's answers match the correct options
         const isCorrect = 
-          correctOptionIds?.length === userResult.options_ids.length &&
-          correctOptionIds?.every((id) => userResult.options_ids.includes(id));
+          correctOptionIds.length === userResult.options_ids.length &&
+          correctOptionIds.every((id) => userResult.options_ids.includes(id || ''));
         // If the answer is correct, add the question's points to the score
         if (isCorrect) {
           score =score +1;
@@ -36,5 +36,3 @@ import { IQuizResults } from "../components/Course/CourseQuizzes";
   };
 
   export default CalculateScore;
-
-  
